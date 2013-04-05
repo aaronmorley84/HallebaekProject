1d1f9124ae369bac501cf999a88998ef7f717d82
Andrew <Andrew@10.99.16.200>
2013-04-05 10:01:24 +0200

    Customer constructor built

--- src/Customer.java
+++ src/Customer.java
@@ -5,8 +5,64 @@
 
 /**
  *
- * @author Kris
+ * @author Andrew, date 05-04-2013, v0.01
  */
 public class Customer {
+    private int customerID,phoneID;
+    private String name,adress,email;
+    
+    public Customer(int customerid,int phoneid, String name, String adress, String email){
+        this.adress = adress;
+        this.customerID = customerid;
+        this.phoneID = phoneid;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public int getPhoneID() {
+        return phoneID;
+    }
+
+    public void setPhoneID(int phoneID) {
+        this.phoneID = phoneID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    @Override
+    public String toString(){
+        
+        return "Customer name: "+ getName()+ "CustomerID: " + getCustomerID()+ "Email: " + getEmail() + "Adress: " + getAdress();
+    }
     
 }
