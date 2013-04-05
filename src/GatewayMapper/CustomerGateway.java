cf4da4339b70e16413623a2ee32b0107fead692c
Andrew <Andrew@10.99.16.200>
2013-04-05 11:19:12 +0200

    CustomerGateway

--- src/GatewayMapper/CustomerGateway.java
+++ src/GatewayMapper/CustomerGateway.java
@@ -3,29 +3,62 @@
  * and open the template in the editor.
  */
 package GatewayMapper;
+
 import Resources.Customer;
 import DBConnection.ConnectionTools;
+import Resources.Product;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
 /**
  *
- * @author Andrew
+ * @author Foxy, 05-04-31, v0.01
  */
 public class CustomerGateway {
-   
+
     ArrayList<Customer> listOfCustomers = new ArrayList<>();
-    
-    
-/*Used to build a list of customers. */
-public void buildCustomerList(){
-Connection con = ConnectionTools.getInstance().getCurrentConnection();
-String SQLStatment1 = "Select * from Customer";
+
+    /*Used to build a list of customers. */
+    public boolean buildCustomerList() {
+        boolean success = false;
+        Connection con = ConnectionTools.getInstance().getCurrentConnection();
+        String SQLString1 = "SELECT * " 
+                + "FROM customers ";
 
 
+        PreparedStatement statement = null;
+        try {
+            statement = con.prepareStatement(SQLString1);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                listOfCustomers.add(new Customer(
+                        rs.getInt(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getInt(4),
+                        rs.getString(5)));
+            }
+            success = true;
+        } catch (Exception e) {
+            System.out.println("Something wrong build" + e.getMessage());
+            success = false;
+        } finally {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                System.out.println("Statment close error\n" + e.getMessage());
+            }
+        }
 
-}//end of buildCustomer
+        return success;
+    }//end of buildCustomer
 
-
-
-
+    public void printList() {
+        for (int i = 0; i < listOfCustomers.size(); i++) {
+            System.out.println(listOfCustomers.get(i).toString());
+        }
+    }
 }
