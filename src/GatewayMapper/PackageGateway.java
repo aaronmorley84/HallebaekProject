750453303f2b535f95953f18682b472c9867b2d3
Andrew <Andrew@10.99.16.200>
2013-04-10 13:24:58 +0200

    Array for packages buildt

---
+++ src/GatewayMapper/PackageGateway.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GatewayMapper;
+import Resources.Package;
+import DBConnection.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+/**
+ *
+ * @author Andrew and Aaron, v0.01 10-04-2013
+ */
+public class PackageGateway {
+    
+    ArrayList<Package>packageList = new ArrayList<Package>();
+   
+    //This method builds an arrayList of Packages. 
+    public boolean buildPackageList(){
+        boolean succes = false;
+       Connection con = ConnectionTools.getInstance().getCurrentConnection();
+        String SQLString1 = "SELECT * " 
+                + "FROM Packages ";
+
+
+        PreparedStatement statement = null;
+       try{
+           statement = con.prepareStatement(SQLString1);
+           ResultSet rs = statement.executeQuery();
+           while(rs.next()){
+               packageList.add(new Package(
+                       rs.getInt(1),
+                       rs.getInt(2),
+                       rs.getString(3),
+                       rs.getString(4)
+                       ));
+           }
+           succes = true;
+       }catch(Exception e){
+           System.out.println("Something went wrong building Packagelist" + e.getMessage());
+           succes = false;
+       }
+       finally{
+           try{
+               statement.close();
+           }catch(SQLException e){
+               System.out.println("Could'nt close Package connection: " + e.getMessage());
+           }
+       }
+       return succes;
+    }//end of buildPackageList
+    
+}//end of PackageGateway
