896a3713b76cc0baf50198a2b527fe8fb676e7a9
Kris <Kris@10.99.16.211>
2013-04-15 10:53:41 +0200

    Kris 15-04-2013 additions for US3

--- src/GatewayMapper/ControllerInterface.java
+++ src/GatewayMapper/ControllerInterface.java
@@ -36,7 +36,6 @@ public interface ControllerInterface {
     boolean getTrucks();
     boolean bookTrucks(int trucksForOrder);
     int getUniqueOrderID();
-    boolean checkTruckAvailability(int startDate);
     boolean getOrders();
     int getOrderListSize();
             
@@ -55,4 +54,6 @@ public interface ControllerInterface {
     boolean addPackage(String name, String description, int price);
     String printPackageList();
     
+    /*CheckerGateway methods*/
+    boolean checkTruckAvailability(int truckID, String date);
 }
