9ac245d5da6ffb75e7205e0c49d6c2a78b1d4d50
Adrian <Adrian@10.99.16.206>
2013-04-17 13:45:25 +0200

    17.04.2013 ---- Added new class for 9.3

---
+++ src/GatewayMapper/OrderBalanceGateway.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GatewayMapper;
+
+import Resources.Order;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Adrian
+ */
+public class OrderBalanceGateway {
+   private ArrayList<Order> orders;
+    
+    public OrderBalanceGateway(ArrayList<Order> orders){
+    this.orders = orders;
+} 
+    
+    public 
+}
